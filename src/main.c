ac0027c6e545a80c9cd67a56fde688bcefcb44ad
maguzzz <markusm.touray@gmail.com>
2026-06-30 12:23:57 +0200

    ip change

--- src/main.c
+++ src/main.c
@@ -50,7 +50,7 @@ int main()
 
     if(isClient){
 
-        Client_Connect("10.253.89.4", 5000, socketFD);
+        Client_Connect("172.20.10.2", 5000, socketFD);
 
         
         char message[1000];
